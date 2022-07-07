3ef5c0d572882fb3d938b18dc530ab54fb25265e
Bhautik Godhani <Bhautikgodhani123@gmail.com>
2022-07-07 12:41:31 +0530

    test

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/epaperweekly'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
-    '/dist/index.html'));
+    '/dist/epaperweekly/index.html'));
 });
 app.listen(process.env.PORT || 8080);
